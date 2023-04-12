4b71183e73e732fb518d1ca270665069f7311008
dpp17 <durga123q.com>
2023-04-12 08:33:43 +0530

    Added:: Setting up Project Using Spring initializer

---
+++ src/main/java/com/bridgelabz/employeePay/EmployeePayApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayApplication.class, args);
+	}
+
+}
