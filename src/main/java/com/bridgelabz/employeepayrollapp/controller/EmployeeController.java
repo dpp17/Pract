e6054edb48373a0186092431d742d6cf3981bf4b
dpp17 <durga123q.com>
2023-04-13 06:01:23 +0530

    Added:: Ability to Store Employee in List

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 import com.bridgelabz.employeepayrollapp.model.EmployeeData;
 import com.bridgelabz.employeepayrollapp.services.IEmployeeServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
     @Autowired
     private IEmployeeServices iEmployeeServices;
 
     @PostMapping("/add")
-    public EmployeeData addEmployee(@RequestBody EmployeeDTO employeeDTO){
+    public ResponseDTO addEmployee(@RequestBody EmployeeDTO employeeDTO){
         EmployeeData empData = iEmployeeServices.addEmployee(employeeDTO);
-        return empData;
+        ResponseDTO responseDTO = new ResponseDTO("Data added Successfully", empData);
+        return responseDTO;
     }
 }
